a2fff6676ab41dfd13b4d84cb03db262ad9e5583
Alexis Carranza <alexis.carranza@claro.com.ar>
2019-08-26 14:09:40 -0300

    Avanzando Login

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,11 +1,36 @@
 import React, {Component} from 'react';
-
 import SignUp from '../components/SignUp';
 
 export default class SignUpScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    setEmail = (email) => {
+        this.setState({
+            email: email
+        })
+    }
+    
+    setPassword = (password) => {
+        this.setState({
+            password: password
+        })
+    }
+    createUser = () =>{
+        console.log(this.state);
+    }
+
     render(){
         return(
-            <SignUp />
+            <SignUp setPassword={this.setPassword} 
+                    setEmail={this.setEmail}
+                    createUser={this.createUser}
+            />
         );
     }
 }
